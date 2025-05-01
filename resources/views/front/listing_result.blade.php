186e3ab2761e4c0faef130f90e89ff41746f5f33
Md Tanzib Hossain <md.tanzib.hossain@gmail.com>
2025-05-01 19:30:17 +0600

    undoing the last commit

--- resources/views/front/listing_result.blade.php
+++ resources/views/front/listing_result.blade.php
@@ -42,7 +42,7 @@
                             @foreach($listing_locations as $row)
                                 <div>
                                     <label>
-                                        <input type="checkbox" name="location" value="{{ $row->id }}" {{ is_array(request()->location) && in_array($row->id, request()->location) ? 'checked' : '' }}>
+                                        <input type="checkbox" name="location[]" value="{{ $row->id }}" {{ is_array(request()->location) && in_array($row->id, request()->location) ? 'checked' : '' }}>
                                         {{ $row->listing_location_name }}
                                     </label>
                                 </div>
@@ -55,7 +55,7 @@
                             @foreach($amenities as $row)
                                 <div>
                                     <label>
-                                        <input type="checkbox" name="amenity" value="{{ $row->id }}" {{ is_array(request()->amenity) && in_array($row->id, request()->amenity) ? 'checked' : '' }}>
+                                        <input type="checkbox" name="amenity[]" value="{{ $row->id }}" {{ is_array(request()->amenity) && in_array($row->id, request()->amenity) ? 'checked' : '' }}>
                                         {{ $row->amenity_name }}
                                     </label>
                                 </div>
